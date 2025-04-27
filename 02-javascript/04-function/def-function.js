3b8953c44fd25ada18e357a973f23c1f84cc425a
MineHayato <ht23a102@oecu.jp>
2025-04-27 03:05:05 +0000

    関数宣言文と関数式による関数の定義

---
+++ 02-javascript/04-function/def-function.js
@@ -0,0 +1,15 @@
+// 関数宣言文
+function double(n) {
+    return n * 2;
+}
+
+// 関数式
+const dbl = function (n) {
+    return n * 2;
+};
+
+const a = double(2);
+console.log(a);
+
+const b = dbl(3);
+console.log(b);
