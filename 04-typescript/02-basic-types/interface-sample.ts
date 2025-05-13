aa8dc691e947dbf4fdcd1ad645ce06bbf990850f
MineHayato <ht23a102@oecu.jp>
2025-05-13 09:28:12 +0000

    インターフェースでオブジェクトの型を指定

---
+++ 04-typescript/02-basic-types/interface-sample.ts
@@ -0,0 +1,10 @@
+interface Color {
+    readonly rgb: string;
+    opacity: number,
+    alias?: string;
+}
+
+const tq: Color = { rgb: '00afcc', opacity: 1};
+tq.alias = 'turquoise';
+console.log(tq);
+// tq.rgb = '03c1ff';
