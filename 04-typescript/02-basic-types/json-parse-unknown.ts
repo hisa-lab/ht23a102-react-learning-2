cab8e8599aad9b0308929e42dad798e412dfdb22
MineHayato <ht23a102@oecu.jp>
2025-05-14 10:49:59 +0000

    エラー箇所のコメントアウト

--- 04-typescript/02-basic-types/json-parse-unknown.ts
+++ 04-typescript/02-basic-types/json-parse-unknown.ts
@@ -1,4 +1,6 @@
-const str2 = `{ "id": 2, "username": "diana" }`;
-const user2: unknown = JSON.parse(str2);
+{
+    const str2 = `{ "id": 2, "username": "diana" }`;
+    const user2: unknown = JSON.parse(str2);
 
-console.log(user2.id, user2.address.zipCode);
+    // console.log(user2.id, user2.address.zipCode);
+}
