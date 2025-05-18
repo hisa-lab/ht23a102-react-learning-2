ad77ab1977c4c064523271de702b5dcf6163c069
MineHayato <ht23a102@oecu.jp>
2025-05-18 10:05:13 +0000

    Rectangleクラスから型を抽出して適用

---
+++ 04-typescript/03-function-class/implements.ts
@@ -0,0 +1,31 @@
+{
+    interface shape {
+        readonly name: string;
+        getArea: () => number;
+    }
+
+    interface Quadrangle {
+        sideA: number;
+        sideB?: number;
+        sideC?: number;
+        sideD?: number;
+    }
+
+    class Rectangle implements shape, Quadrangle {
+        readonly name = 'rectangle';
+        sideA: number;
+        sideB: number;
+
+        constructor(sideA: number, sideB: number) {
+            this.sideA = sideA;
+            this.sideB = sideB;
+        }
+
+        getArea() {
+            return this.sideA * this.sideB;
+        }
+    }
+
+    const ra = new Rectangle(10, 15);
+    console.log(ra.getArea());
+}
