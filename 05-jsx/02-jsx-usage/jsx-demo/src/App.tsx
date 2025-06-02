f887db67a49297fc5e5fbc2fbff90cafe24ceca1
MineHayato <ht23a102@oecu.jp>
2025-06-02 10:39:37 +0000

    Summaryタグを追加

--- 05-jsx/02-jsx-usage/jsx-demo/src/App.tsx
+++ 05-jsx/02-jsx-usage/jsx-demo/src/App.tsx
@@ -1,4 +1,5 @@
 import Greet from './components/Greet';
+import Summary from './components/Summary';
 
 import './App.css';
 import type React from 'react';
@@ -6,6 +7,16 @@ import type React from 'react';
 const App: React.FC = () => (
   <div className="App">
     <Greet name="Patty" times={4} />
+    <Summary title="Maple Town">
+      <p>
+        Patty Hope-rabbit, along with her family, arrives in Maple Town, a
+        smalltown inhabited by friendly animals.
+      </p>
+      <p>
+        Soon, the Rabbit Family settles in Maple Town as mail carriers and the
+        bitter, yet sweet friendship of Patty and Bobby begins to blossom.
+      </p>
+    </Summary>
   </div>
 );
 
