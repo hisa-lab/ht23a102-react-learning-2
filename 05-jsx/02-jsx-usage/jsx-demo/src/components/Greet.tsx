8224d8730d5b8cbe93e24bcbaf2d8e26208c8e00
MineHayato <ht23a102@oecu.jp>
2025-06-02 10:14:25 +0000

    Greetコンポーネントの定義

---
+++ 05-jsx/02-jsx-usage/jsx-demo/src/components/Greet.tsx
@@ -0,0 +1,17 @@
+import type React from "react";
+
+type Props = { name: string; times?: number };
+
+const Greet: React.FC<Props> = ({ name, times=1 }) => {
+    // const { name, times = 1 } = props;
+
+    return (
+        <>
+            {[...Array(times)].map(() => (
+                <p>Hello, {name}!</p>
+            ))}
+        </>
+    );
+};
+
+export default Greet;
