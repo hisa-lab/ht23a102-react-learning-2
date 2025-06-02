7d113c8d3fe9a0023a017a49dc700f7ea542d575
MineHayato <ht23a102@oecu.jp>
2025-06-02 13:46:57 +0000

    select とtextarea コンポーネントのvalue 属性

--- 05-jsx/02-jsx-usage/jsx-demo/src/App.tsx
+++ 05-jsx/02-jsx-usage/jsx-demo/src/App.tsx
@@ -17,6 +17,19 @@ const App: React.FC = () => (
         bitter, yet sweet friendship of Patty and Bobby begins to blossom.
       </p>
     </Summary>
+
+    <form>
+      <label htmlFor="favChar">好きなキャラクターは？</label>
+      <select id="favCahr" defaultValue="fox">
+        <option value="rabbit">パティ</option>
+        <option value="bear">ボビー</option>
+        <option value="fox">ダイアナ</option> {/* selected */}
+        <option value="pig">プリプリン</option>
+        <option value="squirrel">ジュディ</option>
+      </select>
+      <label htmlFor="favReason">そのキャラクターのどこが好き？</label>
+      <textarea id="favReason" defaultValue=" 【例】見た目が好き" />
+    </form>
   </div>
 );
 
