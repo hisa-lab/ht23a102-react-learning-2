0c3115a466f75b763f95005e09ac1172b0695f6c
MineHayato <ht23a102@oecu.jp>
2025-06-09 11:01:13 +0000

    ESlintによる違反の修正

--- 07-lint/01-eslint/src/App.tsx
+++ 07-lint/01-eslint/src/App.tsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
+import { FC, useState } from 'react'
+import viteLogo from 'vite.svg'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 const title = import.meta.env.VITE_APP_TITLE
 console.dir(import.meta.env)
 
-function App() {
+const App: FC = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
